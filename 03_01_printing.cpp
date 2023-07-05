5817eacd604c0b7273029745c5880eaf56740b2d
AhmedAlyEl-Ghannam <ahmedaly.g112@gmail.com>
2023-07-05 14:43:33 +0300

    Finished printing exercises + Homework

---
+++ 03_01_printing.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "It " << "was " << "me, " << "Barry!" << endl;
+	cout << "Hoooo!"
+		 << endl
+		 << "You are approaching me?!\n";
+		 
+	cout << "\n\n";
+	
+	cout << "I can't beat the shit out of you without getting closer!" << endl;
+	
+	cout << "\n\n";
+	
+	cout << "HOHOO! " << "Then come as close as you want!\n";
+	
+	return 0;
+}
